696b12d649d733345313ca5caa1795d9beb54e61
kevinn-h <kevinh449@nycstudents.net>
2026-09-22 09:22:42 -0400

    unit 2 1st commit

---
+++ data.py
@@ -0,0 +1,14 @@
+""" x = 6
+y= float(x)
+
+print(x,y) """
+""" 
+values = [1,2,5,7,2,42,25]
+print(values)
+for i in values:
+    print(i) """
+
+values = [1,2,5,7,2,42,25]
+print(values)
+print(values[0])
+print(values[6])
