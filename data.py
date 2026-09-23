ac1effb293a74de60530d7c31f42f497dc28a056
kevinn-h <kevinh449@nycstudents.net>
2026-09-23 09:25:50 -0400

    2nd commit

--- data.py
+++ data.py
@@ -8,7 +8,41 @@ print(values)
 for i in values:
     print(i) """
 
-values = [1,2,5,7,2,42,25]
+""" values = [1,2,5,7,2,42,25]
 print(values)
 print(values[0])
 print(values[6])
+ """
+
+
+""" #integer
+x = 7
+#string
+name = "kevin"
+#boolean 
+isValid = True
+#float
+bill = 12.22
+#list
+students = ["aiden", "kevin", "william", "nathaniel"]
+students.append("david")
+
+for student in students:
+    if student == "aiden":
+        print(f'we found {student}')
+
+#string 
+y = input("money?")
+z = y + 5 """
+
+""" x = "Hello, my name is Chatgpt."
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+sentence = int(input("give me a sentence NOW"))
+y = sentence.split( )
+if sentence = 
+print(y)
+print(z)
